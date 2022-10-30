5b4272bf9d4470ed25569188dfc5c7961356058d
Mateen Mirani <mateenmirani125@gmail.com>
2022-10-30 13:00:46 +0500

    Serve the static website using Express

--- ExpressJS/Index.js
+++ ExpressJS/Index.js
@@ -1,35 +1,21 @@
 import express from "express";
+import url from "url";
+import path from "path";
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// Create a route for home page
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const staticPath = path.join(__dirname, "./public");
+
+// Serve the static website using Express static method
+app.use(express.static(staticPath));
+
+// Create a route for static web page
 app.get("/", (req, res) => {
   res.send("<h1>Hello Mateen from home page</h1>");
 });
 
-// Create a route for About page
-app.get("/about", (req, res) => {
-  // Send a simple JS object but Nodejs BTs convert it to a JSON 
-  res.send([
-    {
-      id: 1,
-      firstName: "Mateen",
-      lastName: "Nawaz",
-    },
-    {
-      status: "Mingle",
-      passion: "Programming",
-      qualification: "Graduate",
-    },
-  ]);
-});
-
-// Create a route for contact page
-app.get("/contact", (req, res) => {
-  res.send("<h1>Hello Mateen contact page</h1>");
-});
-
 app.listen(port, () => {
-  console.log(`Express server listening on ${port}`);
+  console.log(`Dummy app is listening from port ${port}`);
 });
