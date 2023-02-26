83051a3083635ebc5df337f868321ec729b038fb
Mateen Mirani <mateenmirani125@gmail.com>
2023-02-26 21:23:06 +0500

    Day #5

--- Nodejs_Udemy_course/express/index.js
+++ Nodejs_Udemy_course/express/index.js
@@ -3,10 +3,24 @@ const express = require("express");
 const port = 4000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hey, Mateen</h1>");
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hey, Mateen</h1>");
+// });
+
+// Use Middleware
+// app.use((req, res) => {
+//   res.send("<h1>Hello Mateen Mirani!</h1>");
+// });
+
+// 1. Route
+app.use("/", (req, res, next) => {
+  console.log("First Middleware!");
+  next();
 });
 
+app.use('/add-product', (req, res) => {
+  res.send("<h1>Hello My name is Mateen Nawaz!</h1>")
+})
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
