307b775909dd45c0916b72d542ed29bbec760d58
Mateen Mirani <mateenmirani125@gmail.com>
2023-02-27 21:25:25 +0500

    Day #6 with express

--- Nodejs_Udemy_course/express/index.js
+++ Nodejs_Udemy_course/express/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 const port = 4000;
 const app = express();
@@ -12,15 +16,13 @@ const app = express();
 //   res.send("<h1>Hello Mateen Mirani!</h1>");
 // });
 
-// 1. Route
-app.use("/", (req, res, next) => {
-  console.log("First Middleware!");
-  next();
-});
+// Middleware used for parsing a request body
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res) => {
-  res.send("<h1>Hello My name is Mateen Nawaz!</h1>")
-})
+// Routes
+app.use("/api", adminRoute);
+app.use(shopRoute);
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
