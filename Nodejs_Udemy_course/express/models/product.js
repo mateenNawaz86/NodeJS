6905e11f2f661b926a361b1ffed7a4e32c13a247
Mateen Mirani <mateenmirani125@gmail.com>
2023-03-21 16:26:38 +0500

    refactor some code

--- Nodejs_Udemy_course/express/models/product.js
+++ Nodejs_Udemy_course/express/models/product.js
@@ -1,7 +1,22 @@
 const path = require("path");
 const fs = require("fs");
 
-// const products = []; // empty array of product
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProdsFromFile = (cb) => {
+  // read data from file
+  fs.readFile(p, (error, fileContent) => {
+    if (error) {
+      cb([]);
+    } else {
+      cb(JSON.stringify(fileContent));
+    }
+  });
+};
 
 // Class for managing all incoming products
 module.exports = class Product {
@@ -11,22 +26,7 @@ module.exports = class Product {
 
   // Method to save new product to array
   save() {
-    // products.push(this);
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    ); // it will return newly created json file absolute path
-
-    // Read the content of newly created file
-    fs.readFile(p, (error, fileContent) => {
-      let products = []; // empty products array
-
-      // If there is no error then convert the incoming JSON data into JS object
-      if (!error) {
-        products = JSON.parse(fileContent); // Convert json data into js object or array
-      }
-
+    getProdsFromFile((products) => {
       // Push newly created product to products array which is stored in file
       products.push(this); // here 'this' refer to the class object
 
@@ -39,21 +39,6 @@ module.exports = class Product {
 
   // Method to fetch all products from array
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-
-    // read data from file
-    fs.readFile(p, (error, fileContent) => {
-      if (error) {
-        // return [];
-        cb([]);
-      }
-
-      // return JSON.stringify(fileContent);
-      cb(JSON.stringify(fileContent));
-    });
+    getProdsFromFile(cb);
   }
 };
